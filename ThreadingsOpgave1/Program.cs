a6b479f94e58892151d210f2f1afd4cd32e3aa00
Sebgineer <sebastian-karlsen@hotmail.com>
2021-11-01 09:28:37 +0100

    Add project files.

---
+++ ThreadingsOpgave1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadingsOpgave1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tråd tråd = new Tråd();
+            Thread thread1 = new Thread(new ThreadStart(tråd.Process));
+            Thread thread2 = new Thread(new ThreadStart(tråd.Process2));
+            thread1.Start();
+            thread2.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ThreadingsOpgave1/Tråd.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace ThreadingsOpgave1
+{
+    class Tråd
+    {
+        public void Process()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("C#-trådning er nemt!");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void Process2()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Også med flere tråde ...");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
